feba6b7b800be31ad49e61aecef44108d710f394
Av-Akshay <akshuchauhan1jan@gmail.com>
2023-11-18 21:32:04 -0800

    style: reresponsive

--- src/components/image-section/ImageSec.jsx
+++ src/components/image-section/ImageSec.jsx
@@ -7,7 +7,9 @@ const ImageSec = () => {
         The most <span> professional </span> <br /> way to build <br /> your
         Home
       </h2>
-      <button className="btn"> Use Smart Cost Estimator </button>
+      <div className="first_section_btn">
+        <button className="btn"> Use Smart Cost Estimator </button>
+      </div>
     </div>
   );
 };
